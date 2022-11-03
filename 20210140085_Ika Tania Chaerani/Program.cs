8a2d14b9bfcf7a208df694dffa34cf7434c6be4e
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2022-11-03 15:59:34 +0700

    make array with minimum1 and maximum 33

--- 20210140085_Ika Tania Chaerani/Program.cs
+++ 20210140085_Ika Tania Chaerani/Program.cs
@@ -11,5 +11,19 @@ namespace _20210140085_Ika_Tania_Chaerani
         int[] arr = new int[33];
         int n;
         int i;
+
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("enter the number of elemnt in the array");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 33))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 33 elements.\n");
+            }
+        }
     }
 }
