55ed4819925e9fc1396f7fb8b2781dbb9db27529
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2022-11-03 17:05:35 +0700

    method untuk merge sort

--- 20210140085_Ika Tania Chaerani/Program.cs
+++ 20210140085_Ika Tania Chaerani/Program.cs
@@ -125,8 +125,27 @@ namespace _20210140085_Ika_Tania_Chaerani
                     case 2:
                         Console.WriteLine("");
                         Console.WriteLine("Merge Sort");
-                        p.input();
-                        p.display();
+                        Console.Write("\nProgram for sorting a numeric array using Merge Sorting");
+                        Console.Write("\n\nEnter number of elements: ");
+                        int max = Convert.ToInt32(Console.ReadLine());
+                        int[] numbers = new int[max];
+                        for (int i = 0; i < max; i++)
+                        {
+                            Console.Write("\nEnter [" + (i + 1).ToString() + "] element: ");
+                            numbers[i] = Convert.ToInt32(Console.ReadLine());
+                        }
+                        Console.Write("Input int array: ");
+                        Console.Write("\n");
+                        for (int k = 0; k < max; k++)
+                        {
+                            Console.Write(numbers[k] + " ");
+                            Console.Write("\n");
+                        }
+                        Console.WriteLine("Sorted Array");
+                        MainMerge(numbers, 0, max - 1);
+                        for (int i = 0; i < max; i++)
+                            Console.WriteLine(numbers[i]);
+                        Console.ReadLine();
                         break;
                     case 3:
                         Console.WriteLine("Exit");
