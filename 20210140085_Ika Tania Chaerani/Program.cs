682e93787086fcdfdb302aac919a00114d5dd419
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2022-11-03 15:42:39 +0700

    Add project files.

---
+++ 20210140085_Ika Tania Chaerani/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20210140085_Ika_Tania_Chaerani
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
